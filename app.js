4824fba4299cd9a46359521cc70fe35dd6e841e3
Matthew <matthew@stateofmind.agency>
2020-01-20 10:15:11 +0000

    Render asteroids API working.

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ let today = new Date()
 const pictureOfTheDayURI = 'https://api.nasa.gov/planetary/apod?api_key=Eja2wKokHAlZqpnulA7GXhMyRgJhp7saFdZlqLxe'
 const asteroidsURI = `https://api.nasa.gov/neo/rest/v1/feed?start_date=${today.getFullYear()}-0${(today.getMonth()+1)}-0${today.getDate()}&end_date=${today.getFullYear()}-${(0 + today.getMonth()+1)}-${today.getDate() + 1}&api_key=Eja2wKokHAlZqpnulA7GXhMyRgJhp7saFdZlqLxe`
 const imageSection = document.querySelector('.iotd')
+const asteroidContainer = document.querySelector('.asteroid')
 
 // api
 
@@ -27,8 +28,30 @@ function createImageOfTheDay(image) {
     imageSection.append(h2, img, p)
 }
 
+function createAsteroid(asteroid) {
+    const p = document.createElement('p')
+    p.textContent = asteroid.name
+
+    const img = document.createElement('img')
+    img.classList.add('asteroid-icon')
+    if(asteroid.is_potentially_hazardous_asteroid === true) {
+        img.src = '/images/asteroid_danger.svg'
+    } else {
+        img.src = '/images/asteroid_safe.svg'
+    }
+    
+    asteroidContainer.append(p, img)
+}
+
 function renderImage() {
     get(pictureOfTheDayURI).then(createImageOfTheDay)
+}
+
+function renderAsteroids() {
+    get(asteroidsURI).then(obj => {
+        let asteroids = obj.near_earth_objects[Object.keys(obj.near_earth_objects)[0]].concat(obj.near_earth_objects[Object.keys(obj.near_earth_objects)[1]])
+        asteroids.forEach(createAsteroid)
+    })
 }
 
 
