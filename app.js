8ea5fc5c537fa32a75059648c92937b906640407
Matthew <matthew@stateofmind.agency>
2020-01-20 09:30:05 +0000

    Image of the day API working.

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 // constants 
 
+let today = new Date()
 const pictureOfTheDayURI = 'https://api.nasa.gov/planetary/apod?api_key=Eja2wKokHAlZqpnulA7GXhMyRgJhp7saFdZlqLxe'
+const asteroidsURI = `https://api.nasa.gov/neo/rest/v1/feed?start_date=${today.getFullYear()}-0${(today.getMonth()+1)}-0${today.getDate()}&end_date=${today.getFullYear()}-${(0 + today.getMonth()+1)}-${today.getDate() + 1}&api_key=Eja2wKokHAlZqpnulA7GXhMyRgJhp7saFdZlqLxe`
+const imageSection = document.querySelector('.iotd')
 
 // api
 
@@ -10,6 +13,23 @@ function get(url) {
 
 
 // functions
+
+function createImageOfTheDay(image) {
+    const h2 = document.createElement('h2')
+    h2.textContent = image.title
+
+    const img = document.createElement('img')
+    img.src = image.url
+
+    const p = document.createElement('p')
+    p.textContent = image.explanation
+
+    imageSection.append(h2, img, p)
+}
+
+function renderImage() {
+    get(pictureOfTheDayURI).then(createImageOfTheDay)
+}
 
 
 // event listeners
